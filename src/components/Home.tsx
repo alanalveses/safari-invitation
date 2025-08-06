a00fc8fab79f1367fa5e6caa5216e33ae941af09
alanalveses <alan.alveses@gmail.com>
2025-08-06 18:46:39 -0300

    feat: configure Tailwind CSS and initial setup of the Next.js project

---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
+      Tailwind funcionando 🎉
+    </main>
+  );
+}
